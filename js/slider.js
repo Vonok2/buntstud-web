95db81453fb383433afd3eef232fdf88e04b18df
VLADisLOVE0-0 <vlad.konovalov.web@gmail.com>
2025-11-09 02:46:52 +1000

    Добавлены: Секция hero, Секция почему мы, Секция новостей, а также обновлён header

---
+++ js/slider.js
@@ -0,0 +1,21 @@
+const slides = document.querySelectorAll('.slide');
+const prevBtn = document.querySelector('.slider-arrow-left');
+const nextBtn = document.querySelector('.slider-arrow-right');
+
+let currentSlide = 0;
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.classList.toggle('active', i === index);
+  });
+}
+
+prevBtn.addEventListener('click', () => {
+  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+  showSlide(currentSlide);
+});
+
+nextBtn.addEventListener('click', () => {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+});
